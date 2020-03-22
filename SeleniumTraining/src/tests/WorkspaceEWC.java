7769fcd222328743f3b1d3353f36d67b45991819
Administrator <Administrator@SERVER154.aoc.com>
2020-03-22 17:14:41 +0700

    update

--- SeleniumTraining/src/tests/WorkspaceEWC.java
+++ SeleniumTraining/src/tests/WorkspaceEWC.java
@@ -1,6 +1,7 @@
 package tests;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -19,21 +20,22 @@ import org.testng.annotations.Test;
 public class WorkspaceEWC {
 	// Variable
 	WebDriver driver;
-	ArrayList<String> windows;
+	Set<String> windows;
+	Iterator<String> itr;
 	String wsURL = "http://100.30.5.92:31380/Login/?returnpage=../services/UnifiedAgentController/workspaces/";// "http://100.30.6.137:31380/Login/?returnpage=../services/UnifiedAgentController/workspaces/";
 	String webchatURL = "http://10.30.1.210:81/ewcsite%20-%20mcha576%20link/";// "http://10.30.1.236:8080/ewcsite/";
 	String checkLink = "https://mcha576.aoc.com:8445/CustomerControllerWeb/currentqueue";// "https://autosrv98:8445/CustomerControllerWeb/callback";
 	String username = "aoc\\nvhuy0002"; // "ACC_Huy@automation";
 	String password = "1_Abc_123";
-	String skillset = "WC_Webchat2";//"WC_HUY_1";
+	String skillset = "WC_Webchat2";// "WC_HUY_1";
 	String customerEmail = "huy@gmail.com";
 
 	@Test
 	public void EWC() throws InterruptedException {
 		// Thong so test
 		WebDriverWait waits = new WebDriverWait(driver, 60);
-		windows = new ArrayList<String>(driver.getWindowHandles());
-		
+		windows = driver.getWindowHandles();
+
 		// Login
 		driver.findElement(By.id("username")).sendKeys(username);
 		driver.findElement(By.id("password")).sendKeys(password);
@@ -73,17 +75,12 @@ public class WorkspaceEWC {
 		}
 
 		// current WS tab
-		
-		String mainTab0 = windows.get(0);
-		System.out.println("mainTab0: " + mainTab0);
 
 		// Open EWC tab
 		Thread.sleep(1000);
-		windows = openAndSwitchNewTabs(webchatURL, 1);
 
 		// Check link EWC
 		Thread.sleep(1000);
-		windows = openAndSwitchNewTabs(checkLink, 2);
 
 		try {
 			driver.findElement(By.xpath("//*[@id='details-button']")).click();
@@ -96,7 +93,7 @@ public class WorkspaceEWC {
 		driver.close();
 
 		// return to EWC tab
-		driver.switchTo().window(windows.get(1));
+
 		driver.navigate().refresh();
 		Thread.sleep(1000);
 
@@ -113,7 +110,6 @@ public class WorkspaceEWC {
 
 		// sw to ws tab
 		Thread.sleep(1000);
-		driver.switchTo().window(windows.get(0));
 
 		waits.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ow_card_accept_btn']")));
 
@@ -137,32 +133,31 @@ public class WorkspaceEWC {
 
 	public static void Hover(WebDriver driver, WebElement element) {
 		Actions action = new Actions(driver);
-		action.moveToElement(element).perform();
+		action.moveToElement(element).build().perform();
 	}
 
-	public ArrayList<String> openAndSwitchNewTabs(String URL, int posTab) {
+	public void openTab(String URL) {
 		((JavascriptExecutor) driver).executeScript("window.open()");
-		windows = new ArrayList<String>(driver.getWindowHandles());
-		System.out.println("windows" + windows);
-		driver.switchTo().window(windows.get(posTab));
-		System.out.println("CurrTabID: " + driver.getWindowHandle());
-		driver.get(URL);
-		return windows;
+		windows = driver.getWindowHandles();
+		System.out.println("windows" + windows.toString());
+		String currWindow = driver.getWindowHandle();
+		itr = windows.iterator();
+		while (itr.hasNext()) {
+			String itrWindow = itr.next();
+			if (itrWindow.equalsIgnoreCase(currWindow)) {
+				itrWindow = itr.next();
+				driver.switchTo().window(itrWindow);
+				driver.get(URL);
+				break;
+			}
+		}
+
 	}
+
 	public void EWCpage(String URL, String linkCheck, String skillset) {
-		windows = openAndSwitchNewTabs(webchatURL, 1);
+
 	}
-	
+
 }
 
-//String agentStatus = driver.findElement(By.xpath("//div[@id='ow_Icon_State2']")).getText();
-//System.out.println("agentStatus: " + agentStatus);
-//Hover(driver, driver.findElement(By.xpath("//*[@id='ow_go_not_ready']")));
-//List<WebElement> agentStates = driver
-//		.findElements(By.xpath("//*[starts-with(@id,'menu_container')][2]/md-menu-content/md-menu-item")); // *[starts-with(@id,"menu_container")][2]/md-menu-content/md-menu-item
-//for (WebElement agentState : agentStates) {
-//	System.out.println("agentState: " + agentState.getText());
-//}
-//if (!agentStatus.contains("NOT")) {
-//	agentStates.get(3).click();
 //}
