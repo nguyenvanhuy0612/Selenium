ee3a743514bd983f7f34e59b516276ec920fb3a8
Administrator <Administrator@SERVER154.aoc.com>
2020-03-22 11:27:36 +0700

    d

--- SeleniumTraining/src/tests/WorkspaceEWC2.java
+++ SeleniumTraining/src/tests/WorkspaceEWC2.java
@@ -1,14 +1,18 @@
 package tests;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,54 +20,54 @@ import org.testng.annotations.Test;
 public class WorkspaceEWC2 {
 	// Variable
 	WebDriver driver;
-	String wsURL = "https://google.com";
-	String username = "ACC_Huy@automation";
+	ArrayList<String> windows;
+	List<String> winLists;
+	String wsURL = "http://100.30.5.92:31380/Login/?returnpage=../services/UnifiedAgentController/workspaces/";// "http://100.30.6.137:31380/Login/?returnpage=../services/UnifiedAgentController/workspaces/";
+	String webchatURL = "http://10.30.1.210:81/ewcsite%20-%20mcha576%20link/";// "http://10.30.1.236:8080/ewcsite/";
+	String checkLink = "https://mcha576.aoc.com:8445/CustomerControllerWeb/currentqueue";// "https://autosrv98:8445/CustomerControllerWeb/callback";
+	String username = "aoc\\nvhuy0002"; // "ACC_Huy@automation";
 	String password = "1_Abc_123";
+	String skillset = "WC_Webchat2";// "WC_HUY_1";
+	String customerEmail = "huy@gmail.com";
 
 	@Test
-	public void TEST() throws InterruptedException {
-		String mainTab = driver.getWindowHandle();
-		System.out.println("mainTab: " + mainTab);
-		// driver.findElement(By.tagName("body")).sendKeys(Keys.CONTROL + "t");
-
-		// new tabs
-		((JavascriptExecutor) driver).executeScript("window.open()");
-		ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
-		System.out.println("windows" + windows);
-		driver.switchTo().window(windows.get(1));
-		System.out.println("Tab1: " + driver.getWindowHandle());
-		driver.get(wsURL);
-		Thread.sleep(5000);
-		
-		((JavascriptExecutor) driver).executeScript("window.open()");
+	public void EWC() throws InterruptedException {
+		// Thong so test
+		WebDriverWait waits = new WebDriverWait(driver, 60);
 		windows = new ArrayList<String>(driver.getWindowHandles());
-		System.out.println("windows" + windows);
-		driver.switchTo().window(windows.get(2));
-		System.out.println("Tab1: " + driver.getWindowHandle());
-		driver.get(wsURL);
+		winLists = new ArrayList<String>(driver.getWindowHandles());
+		System.out.println("windows: " + windows);
+		System.out.println("winLists: " + winLists);
 
-//		((JavascriptExecutor) driver).executeScript("window.open()");
-//		Set<String> tabs = driver.getWindowHandles();
-//		System.out.println("List tabs: " + tabs);
-//		Iterator<String> tabsList = tabs.iterator();
 	}
 
 	@BeforeMethod
 	public void Setup() {
 		driver = utilities.DriverFactory.CreateDriver("chrome");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		// driver.manage().window().maximize();
-		driver.get(wsURL);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(webchatURL);
 	}
 
 	@AfterMethod
 	public void TearDown() {
-
+		// driver.close();
 	}
 
 	public static void Hover(WebDriver driver, WebElement element) {
 		Actions action = new Actions(driver);
 		action.moveToElement(element).perform();
+	}
+
+	public void openAndSwitchNewTabs(String URL, String posTab) {
+		((JavascriptExecutor) driver).executeScript("window.open()");
+		
+		driver.get(URL);
+	}
+
+	public void EWCpage(String URL, String linkCheck, String skillset) {
+		
 	}
 
 }
